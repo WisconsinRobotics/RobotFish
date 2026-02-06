3c0b25b31196d561df1020319aa486e32ee329f1
User <user@raspberrypi>
2026-02-06 17:01:41 -0600

    Final folder updates

--- Camera/usbcam.py
+++ Camera/usbcam.py
@@ -1,19 +1,34 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+object_detector = cv2.createBackgroundSubtractorMOG2()
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
+    height, width, _ = frame.shape
+    print(height, width)
 
-    cv2.imshow("USB Camera", frame)
+    blurred = cv2.GaussianBlur(frame, (3, 3), 0)
+    cv2.waitKey(100)
+    #Object_detection
 
-    if cv2.waitKey(1) == 27:  # ESC to quit
+    mask = object_detector.apply(blurred)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        
+        area = cv2.contourArea(cnt)
+        if area < 5000:
+            continue
+        
+        
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0))
+
+    cv2.imshow("usb cam", frame)
+    
+    if cv2.waitKey(1) == 27: #esl to exit
         break
 
 cap.release()
