1fb6228bd6cc4c3b6344d6b2869ede2717e25c4e
gisele <han102081@gmail.com>
2026-03-23 18:54:55 -0500

    test for recognition cam

---
+++ Camera/facerocognitionCam.py
@@ -0,0 +1,54 @@
+import cv2
+import time
+
+
+
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+tracker = None
+tracking = False
+last_detect_time = 0
+detect_interval = 0.5
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+    if tracking and tracker is not None:
+        success, box = tracker.update(frame)
+        if success:
+            x, y, w, h = [int(v) for v in box]
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.putText(frame, "face detected", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        else :
+            tracking = False
+            tracker = None
+
+    if not tracking:
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(
+            gray, 
+            scaleFactor = 1.1, 
+            minNeighbors = 5, 
+            minSize=(60, 60)
+        )
+        if len(faces) > 0:
+            x, y, w, h = max(faces, key=lambda b: b[2] * b[3])
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            cv2.putText(frame, "face_detecting", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+            tracker = cv2.TrackerCSRT_create()
+            tracker.init(frame, (x, y, w, h))
+            tracking = True
+        else: 
+            cv2.putText(frame, "No face detected yet . . .", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+    cv2.imshow("Face recognition tracking~")
+
+cap.release()
+cv2.destroyAllWindows()
